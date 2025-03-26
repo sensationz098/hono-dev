814abae15b5ad19402209fe605aab8be6c6ffb1a
Sensationz Media and Arts Pvt. Ltd. <sensationzdigital@gmail.com>
2025-03-26 17:56:50 +0530

    LLpafdna

--- api/routes/userRouter.ts
+++ api/routes/userRouter.ts
@@ -2,7 +2,9 @@ import { Hono } from "hono";
 
 export const UserRouter = new Hono();
 
-UserRouter.post("/create", (c) => {
-  console.log(c.body);
-  return c.json({ message: "Hello user!" });
+UserRouter.post("/create", async (c) => {
+  const data = await c.req.json();
+
+  console.log(data, "Hello");
+  return c.json({ message: "Hello user!", data: `${data}` });
 });

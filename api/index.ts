edf5a71700f108e58125843bb55a095ffb3b74f1
sensationz098 <sensationzdigital@gmail.com>
2025-03-26 17:30:00 +0530

    init

---
+++ api/index.ts
@@ -0,0 +1,18 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+export const config = {
+  runtime: "edge",
+};
+
+const app = new Hono().basePath("/api");
+
+app.get("/", (c) => {
+  return c.json({ message: "Hello Hono!" });
+});
+
+app.post("/create", (c) => {
+  return c.json({ message: "Hello post request" });
+});
+
+export default handle(app);
